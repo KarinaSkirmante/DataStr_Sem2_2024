1552d8e2127dc4b894975814f9fc75a880d6de4c
unknown <localuser@vea.lv>
2024-04-03 10:03:05 +0300

    Add: add at position

--- src/datastr/MyLinkedList.java
+++ src/datastr/MyLinkedList.java
@@ -44,6 +44,77 @@ public class MyLinkedList<Ttype> {
 		
 	}
 	
+	public void add(Ttype element, int position) throws Exception {
+		if(element == null) throw new Exception("Problems with element");
+		//TODO
+		
+		//1. pārbaude uz position
+		
+		if(position < 0 || position > counter) throw new Exception("Problems with position");
+		
+		//1.5. ja saraksts ir tukšs????
+		if(isEmpty() && position == 0) {
+			
+			add(element);
+			//garais variants:
+			/*MyListNode newNode = new MyListNode(element);
+			first  = newNode;
+			last = newNode;
+			counter++;*/
+		}
+		else if(isEmpty() && position != 0) throw new Exception("Problems with position");
+		
+		
+		
+		//2. pievienot elementu priekšā
+		else if(position == 0) {
+			MyListNode newNode = new MyListNode(element);
+			newNode.setNext(first);
+			first.setPrevious(newNode);
+			first = newNode;
+			counter++;
+			
+		}
+		//3. pielikt elementu beigās
+		else if (position == counter)
+		{
+			add(element);
+			
+			//garais veids
+			/*MyListNode newNode = new MyListNode(element);
+			newNode.setPrevious(last);
+			last.setNext(newNode);
+			last = newNode;
+			counter++;*/
+		}
+		//4. ielikt elementu pa vidu
+		
+		else
+		{
+			MyListNode temp = first;
+			for(int i = 0 ; i < position ; i++) {
+				temp = temp.getNext();
+			}
+			
+			MyListNode rightNode = temp;
+			MyListNode leftNode = temp.getPrevious();
+			MyListNode newNode = new MyListNode(element);
+			
+			newNode.setNext(rightNode);
+			rightNode.setPrevious(newNode);
+			newNode.setPrevious(leftNode);
+			leftNode.setNext(newNode);
+			
+			counter++;
+			
+	
+		}
+		
+		
+		
+	}
+	
+	
 	
 	public void print() throws Exception
 	{
